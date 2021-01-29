967690c531b825aea09354fcdb38ec146e4c66ea
picode7 <9394244+picode7@users.noreply.github.com>
2021-01-29 23:08:17 +0100

    removing old mess

--- Client/functions.js
+++
@@ -1,8 +0,0 @@
-function elementFromHTML(html) {
-    var div = document.createElement('div');
-    div.innerHTML = html;
-    var el = div.firstElementChild;
-    div.removeChild(el);
-    return el;
-}
-//# sourceMappingURL=functions.js.map
